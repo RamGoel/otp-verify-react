2d2efcf4022797ed1fba9c5709428c24646e5f49
RamGoel <rgoel766@gmail.com>
2022-05-23 20:52:51 +0530

    Added Emoticon

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import './Home.css';
 export default function Welcome(props) {
   return (
     <div id="mainDiv">
-      <h2 id="welctext">Welcome, {props.name}</h2>
+      <h2 id="welctext">Welcome, {props.name} :)</h2>
     </div>
   );
 }
